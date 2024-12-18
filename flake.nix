bee4420f5b0a7bbab01f3a6721f8beaa36dd7d88
alexpaniman <alexpaniman@gmail.com>
2024-12-18 17:11:59 +0300

    build: nix flake updated to support cross-compiling

--- flake.nix
+++ flake.nix
@@ -9,19 +9,62 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
+        nativePkgs = import nixpkgs { inherit system; };
+
+        crossSystems = {
+          arm64-linux = { config = "aarch64-linux"; };
+        };
+
+        buildInputs = pkgs: [
+          pkgs.glibc.static
+        ];
+
+        nativeInputs = pkgs: [
+          pkgs.buildPackages.cmake
+        ];
+
+        shellInputs = pkgs: [
+          pkgs.clang-tools
+          pkgs.mold
+          pkgs.ninja
+        ];
+
+        mkDerivation = pkgs: pkgs.stdenv.mkDerivation {
           name = "blockchain";
           src = ./.;
 
-          nativeBuildInputs = with pkgs; [
-            cmake
-            ninja
+          nativeBuildInputs = nativeInputs pkgs;
+          buildInputs = buildInputs pkgs;
 
-            glibc.static
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=MinSizeRel"
+            "-DCMAKE_C_FLAGS=-static"
+            "-DCMAKE_CXX_FLAGS=-static"
           ];
+
+          postInstall = ''
+            ${pkgs.buildPackages.binutils}/bin/${pkgs.stdenv.cc.targetPrefix}strip "$out/bin/blockchain"
+          '';
+        };
+      in
+      {
+        packages = {
+          default = mkDerivation nativePkgs;
+
+          cross = builtins.mapAttrs (crossName: crossSystem:
+            mkDerivation (import nixpkgs {
+              inherit system;
+              inherit crossSystem;
+            })
+          ) crossSystems;
+        };
+
+        devShell = nativePkgs.mkShell {
+          nativeBuildInputs =
+            builtins.concatLists
+              (builtins.map (input: input nativePkgs)
+                [shellInputs buildInputs nativeInputs]
+              );
         };
       }
     );
